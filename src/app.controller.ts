187be4d0c7d67d3f2bb6739697ac4c9eab814996
Viktar Belski <belski.viktor.dev@gmail.com>
2023-08-30 23:33:57 +0300

    docker config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,13 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Post()
-  returnBody(@Body() body: any): string {
-    return {
-      ...body,
-      serverMessage: 'Hello from server',
-    };
-  }
+    @Get()
+    getHello() {
+        return { content: this.appService.getHello() };
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+
 import { AppModule } from './app.module';
+
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:4200',
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
